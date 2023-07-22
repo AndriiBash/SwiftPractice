afa51c6b4065143aefe9a1da930a1b113b7c7e29
Andriy Izbash <104149691+AndriyPetrovic@users.noreply.github.com>
2023-07-22 21:31:42 +0300

    Create 10Lab.swift

---
+++ src/sourceCode/10Lab.swift
@@ -0,0 +1,16 @@
+//-------------------------------------------------------------------------------------------
+//
+//  Project SwiftPractice
+//  Copyright © 2023. All Rights Reserved.
+//
+//  OS:           macOS 13.4 (Ventura)
+//  FILE:         10Lab.swift
+//  AUTHOR:       Andrii Izbash
+//
+//  Project link: https://github.com/AndriyPetrovic/SwiftPractice
+//-------------------------------------------------------------------------------------------
+
+/*
+    Task 1
+
+*/
